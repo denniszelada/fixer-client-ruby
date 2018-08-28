88a5f0194739984334dd940958128ceeef8d5d83
Dennis Zelada <denniszelada@gmail.com>
2018-08-28 14:56:40 +0000

    Add new external api

--- app/fixer.rb
+++ app/fixer.rb
@@ -3,27 +3,34 @@
 require 'httparty'
 
 module Fixer
-  #require 'fixer/config'
-
-  class Fix
+  class CurrencyExchange 
     include HTTParty
-    API_KEY = '94c49fb502e8386615e28526ee044b6c'
-    base_uri "http://data.fixer.io/api/latest?access_key=#{API_KEY}"
+    base_uri "http://frankfurter.app/"
 
-    def initialize()
-      @options = { query: { access_key: API_KEY } }
-    end
-
-    def all
-      self.class.get('/latest', @options)
+    def get_time_series(from, to, based = 'EUR')
+      self.class.get("/#{from}..#{to}?from=#{based}")
     end
   end
 
-  def self.run
-    response = Fix.new.all
+  extend self
+
+  def run
+    result = get_exchange_currencies
+    parse_exchange_currencies(result)
+  end
+
+  def get_exchange_currencies
+    response = CurrencyExchange.new().get_time_series('2018-01-01', '2018-08-08')
     if response.success?
-      p response
+      response.body
     end
   end
 
+  def parse_exchange_currencies(currencies)
+    JSON.parse(currencies)
+  end
+
+  def store_exchange_currencies(exchange_currencies)
+
+  end
 end
